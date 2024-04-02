be66efacc21cc796cab8fe02bb1bd90573087328
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-04-02 16:35:10 +0900

    feat: Car 클래스 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,17 +6,15 @@ public class Application {
     public static void main(String[] args) {
         String[] carNames = readCarNames();
         int tries = readTries();
+
+        Car[] cars = new Car[carNames.length];
+        for (int i = 0; i < carNames.length; i++) {
+            cars[i] = new Car(carNames[i]);
+        }
     }
     private static String[] readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        String[] names = input.split(",");
-        for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("5자 이하만 가능합니다.");
-            }
-        }
-        return names;
+        return Console.readLine().split(",");
     }
     private static int readTries() {
         System.out.println("시도할 회수는 몇회인가요?");
