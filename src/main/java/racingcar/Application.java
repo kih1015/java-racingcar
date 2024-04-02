d26d531a1d02b3c20d09d26dfff797a3fba38fe6
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-04-02 16:26:24 +0900

    feat: 시도횟수 입력 및 유효성 검사 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,9 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String[] carNames = getCarNames();
+        String[] carNames = readCarNames();
+        int tries = readTries();
     }
-    private static String[] getCarNames() {
+    private static String[] readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         String[] names = input.split(",");
@@ -16,5 +17,13 @@ public class Application {
             }
         }
         return names;
+    }
+    private static int readTries() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int tries = Integer.parseInt(Console.readLine());
+        if (tries <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상의 정수여야 합니다.");
+        }
+        return tries;
     }
 }
