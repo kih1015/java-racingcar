21f00df001fffc4ea1aeeed6915e4a14a650d65d
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-04-24 02:58:05 +0900

    refactor: main 메서드 변경

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,10 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        Racing race = new Racing(readNames(), readTry());
+        Racing race = new Racing(new ArrayList<>(), new View());
         race.run();
-        printWinners(race);
-    }
-
-    private static String[] readNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine().split(",");
-    }
-
-    private static int readTry() {
-        System.out.println("시도할 회수는 몇회인가요?");
-        return Integer.parseInt(Console.readLine());
-    }
-
-    private static void printWinners(Racing race) {
-        String winners = String.join(", ", race.getWinnerNames());
-        System.out.println("최종 우승자 : " + winners);
     }
 }
