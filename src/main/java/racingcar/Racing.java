dde44a89524548ca4b6be023f9f33e886ec7ace5
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-04-24 02:56:28 +0900

    refactor: view와 model을 관리하도록 변경

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,37 +1,36 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Racing {
-    final private List<Car> cars = new ArrayList<>();
-    final private int totalTry;
+    private final View view;
+    private final List<Car> cars;
 
-    public Racing(String[] carNames, int totalTry) {
-        for (String carName : carNames) {
-            cars.add(new Car(carName));
-        }
-        this.totalTry = totalTry;
+    public Racing(List<Car> cars, View view) {
+        this.view = view;
+        this.cars = cars;
     }
 
     public void run() {
-        System.out.println("\n실행 결과");
+        for (String carName : view.readNames()) {
+            cars.add(new Car(carName));
+        }
+        int totalTry = view.readTry();
+        view.printIntroForResult();
         for (int i = 0; i < totalTry; i++) {
             TryAdvanceAll();
-            printState();
+            view.printState(cars.stream().map(Car::toString).toList());
         }
+        view.printWinners(getWinnerNames());
     }
 
-    public List<String> getWinnerNames() {
+    private List<String> getWinnerNames() {
         int maxPosition = 0;
-
         // 가장 높은 위치 찾기
         for (Car car : cars) {
             maxPosition = Math.max(maxPosition, car.getPosition());
         }
-
         // 가장 높은 위치에 있는 차량 찾기
         List<String> winnerCarNames = new ArrayList<>();
         for (Car car : cars) {
@@ -39,7 +38,6 @@ public class Racing {
                 winnerCarNames.add(car.getName());
             }
         }
-
         return winnerCarNames;
     }
 
@@ -47,12 +45,5 @@ public class Racing {
         for (Car car : cars) {
             car.TryAdvance();
         }
-    }
-
-    private void printState() {
-        for (Car car : cars) {
-            System.out.println(car);
-        }
-        System.out.println();
     }
 }
