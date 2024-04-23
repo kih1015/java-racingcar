f30e8fe7a1c32cb5af4f7e5f738026b1ce28e5fa
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-04-24 02:42:19 +0900

    refactor: view가 model, controller에 의존하지 않도록 변경

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -2,25 +2,27 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class View {
-    private String[] readNames() {
+    public String[] readNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine().split(",");
     }
 
-    private int readTry() {
+    public int readTry() {
         System.out.println("시도할 회수는 몇회인가요?");
         return Integer.parseInt(Console.readLine());
     }
 
-    private void printWinners(Racing race) {
-        String winners = String.join(", ", race.getWinnerNames());
+    public void printWinners(List<String> winnersNames) {
+        String winners = String.join(", ", winnersNames);
         System.out.println("최종 우승자 : " + winners);
     }
 
-    private void printState(Car[] cars) {
-        for (Car car : cars) {
-            System.out.println(car);
+    public void printState(List<String> state) {
+        for (String carState : state) {
+            System.out.println(carState);
         }
         System.out.println();
     }
